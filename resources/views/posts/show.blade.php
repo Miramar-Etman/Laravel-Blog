680b0e50f01916ccfa4ce01b4d7abe503988026d
Mira <mira.ph.lin@gmail.com>
2019-03-15 06:34:05 +0200

    posts.show bootstrap and styling added

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,15 +1,24 @@
 @extends('layouts.app')
-@section('content')
- <a href="{{route('posts.index')}}" class="btn btn-secondary">Back</a>
 
-    <form >
-  <div class="form-group">
-    <label> {{$post->user->name}} </label>
-    <br>
-    <label  > {{$post->title}} </label>
-     <br>
-    <label > {{$post->description}} </label>
-    <br>
-    <label > {{$post->created_at}} </label>
-  </div>
-</form>
+@section('content')
+<body >
+<h1 style="margin-left:570px;padding-top:10px;"> Post Information </h1>
+
+     <a style="margin-left: 1000px;width:150px;" href="{{route('posts.index')}}" class="btn btn-secondary">Back</a>
+
+      <div class="bg-light text-dark" style="margin-left:500px;width: 500px;height:350px;opacity: 0.9;text-align:center;border-radius: 15px;font-family: Nunito;">
+        <label style="margin-top: 20px;color:red;">Post Title: </label>
+        <label  > {{$post->title}} </label>
+        <br>
+        <label style="color: red;">Post Creator: </label>
+        <label> {{$post->user->name}} </label>
+        <br>
+        <label style="margin-top: 20px;color: red;">Post Description: </label>
+        <br>
+       <textarea disabled style="width:400px;"> {{$post->description}} </textarea>
+       <br>
+        <label style="color:red;margin-top: 20px;">Post Created At: </label>
+        <br>
+        <label > {{$post->created_at}} </label>
+      </div>
+</body>
